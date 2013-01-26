f801391af8febfb056bde6df5f5cfb89ee47bdc5
Johan Mattsson <johan.mattsson.m@gmail.com>
2013-01-26 19:49:27 +0100

    version for dmg file

--- mac/create_dmg.py
+++ mac/create_dmg.py
@@ -5,6 +5,9 @@ import shutil
 import subprocess
 import sys
 
+sys.path.append('../')
+import version
+
 def run(cmd):
 	process = subprocess.Popen (cmd, shell=True)
 	process.communicate()[0]
@@ -13,8 +16,8 @@ def run(cmd):
 		exit(1)
 		
 def create_dmg ():
-	run("rm -rf build/mac/birdfont.dmg")	
-	run("hdiutil create -megabytes 55 -fs HFS+ -volname birdfont build/mac/birdfont")
+	run("rm -rf build/mac/birdfont-" + VERSION + ".dmg")	
+	run("hdiutil create -megabytes 55 -fs HFS+ -volname birdfont build/mac/birdfont-" + VERSION)
 	# run("cp -r build/mac/birdfont.app /Volumes/birdfont/")
 	print ("Copy the files with Finder, we don't know the path here.")
 
