e4c4ea0e2b5e86f50b9f98784bc29f7d379ae427
Johan Mattsson <johan.mattsson.m@gmail.com>
2013-07-01 15:18:33 +0200

    Bugfix 0000010: make birdfont license more clear

--- scripts/bavala.py
+++ scripts/bavala.py
@@ -1,4 +1,21 @@
-"""bavala is a build-tool for Vala based on doit (http://pydoit.org)"""
+"""
+Copyright (C) 2012, 2013 Eduardo Naufel Schettino and Johan Mattsson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""bavala is a build-tool for Vala that uses doit (http://pydoit.org)"""
 
 import glob
 import os
