04189c182eac7d4a82791e9b3876bc9df12e4d28
Johan Mattsson <johan.mattsson.m@gmail.com>
2014-08-07 11:09:03 +0200

    Version 0.45

--- scripts/version.py
+++ scripts/version.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-VERSION = '0.44'
-SO_VERSION_MAJOR = '18'
+VERSION = '0.45'
+SO_VERSION_MAJOR = '19'
 SO_VERSION_MINOR = '0'
 SO_VERSION = SO_VERSION_MAJOR + '.' + SO_VERSION_MINOR
