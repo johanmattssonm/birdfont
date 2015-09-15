b9a2c1c04264f7b4f661e5f9ce3005a39dd4b942
Johan Mattsson <johan.mattsson.m@gmail.com>
2015-09-15 21:54:36 +0200

    Fix build scripts

--- scripts/linux_build.py
+++ scripts/linux_build.py
@@ -4,9 +4,9 @@ from run import run
 
 if not path.isfile('./build/configured'):
 	run("""./configure \
-			  --valac-flags="--pkg gdk-pixbuf-2.0 --pkg gtk+-3.0" \
-			  --cflags="$(pkg-config --cflags gdk-pixbuf-2.0)" \
-			  --ldflags="$(pkg-config --libs gdk-pixbuf-2.0)""")
+			  --valac-flags='--pkg gdk-pixbuf-2.0 --pkg gtk+-3.0' \
+			  --cflags='$(pkg-config --cflags gdk-pixbuf-2.0)' \
+			  --ldflags='$(pkg-config --libs gdk-pixbuf-2.0)'""")
 
 run('./build.py')		  
 print ("Done")
