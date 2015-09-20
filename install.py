2b59835a3f8b167a02665f1f30c1e99fb09d836c
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2015-09-20 13:54:08 +0200

    the "bsd" blocks used "openbsd"-specific variables, so tighten the check

--- install.py
+++ install.py
@@ -140,7 +140,7 @@ if not options.libdir:
 else:
 	libdir = options.libdir
 
-if "bsd" in sys.platform:
+if "openbsd" in sys.platform:
         install ('build/libbirdfont.so.' + '${LIBbirdfont_VERSION}', '/lib', 644)
 elif os.path.isfile ('build/bin/libbirdfont.so.' + version.SO_VERSION):
 	install ('build/bin/libbirdfont.so.' + version.SO_VERSION, libdir, 644)
@@ -158,7 +158,7 @@ else:
 	print ("Can't find libbirdfont.")
 	exit (1)
 
-if "bsd" in sys.platform:
+if "openbsd" in sys.platform:
         install ('build/libbirdgems.so.' + '${LIBbirdgems_VERSION}', '/lib', 644)
 elif os.path.isfile ('build/bin/libbirdgems.so.' + version.LIBBIRDGEMS_SO_VERSION):
         install ('build/bin/libbirdgems.so.' + version.LIBBIRDGEMS_SO_VERSION, libdir, 644)
